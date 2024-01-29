c196b2a2a99ff62a8d9266a9301d5f9fdac1fdd5
Mohit <codermohit17@gmail.com>
2024-01-29 22:21:43 +0530

    This is commit from second user

--- app/src/main/java/com/example/rgpmusic/MainActivity.java
+++ app/src/main/java/com/example/rgpmusic/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //This is First Edited Text
+        //This is Second Edited Text
     }
 }
